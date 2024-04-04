184a46b6b1e1f416fbf7d94aa10bf75c3fd93c62
Isaac PM <isaacpalma21@gmail.com>
2024-04-04 10:27:35 -0600

    Cración de un gráfico para representar el tiempo

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import threading
 import random
+import matplotlib.pyplot as plt
 import time
 
+suma_de_tiempos = 0
 
 def ordenar_subvector(subvector, hilo):
+    global suma_de_tiempos
     tiempo_inicio = time.time()
     subvector.sort()
     tiempo_fin = time.time()
     tiempo_ejecucion = tiempo_fin - tiempo_inicio
-    print(f"Hilo {hilo}: Subvector ordenado (Tiempo: {tiempo_ejecucion} segundos)")
+    suma_de_tiempos += tiempo_ejecucion
+    # print(f"Hilo {hilo}: Subvector ordenado (Tiempo: {tiempo_ejecucion} segundos)")
 
 
 def dividir_vector(vector, num_hilos):
@@ -39,7 +43,31 @@ def ordenar_vector(vector, num_hilos):
     # print(f"Vector ordenado final: {vector_ordenado}")
 
 
-vector_grande = [random.randint(1, 10) for _ in range(1_000_000)]
-num_hilos = int(input("Ingrese la cantidad de hilos: "))
+vector_grande_copia = [random.randint(1, 10) for _ in range(1_000_000)]
+# totales = {}
+# num_hilos = int(input("Ingrese la cantidad de hilos: "))
+num_hilos_list = []
+tiempo_list = []
 
-ordenar_vector(vector_grande, num_hilos)
+for num_hilos in range(1, 300):
+    vector_grande = list(vector_grande_copia)
+    suma_de_tiempos = 0
+    ordenar_vector(vector_grande, num_hilos)
+    print(f"La suma de tiempo total es: {suma_de_tiempos} con {num_hilos} hilos")
+    num_hilos_list.append(num_hilos)
+    tiempo_list.append(suma_de_tiempos)
+
+# Graficar los datos
+plt.plot(num_hilos_list, tiempo_list)
+plt.xlabel('Cantidad de Hilos')
+plt.ylabel('Tiempo')
+plt.title('Tiempo vs Cantidad de Hilos')
+plt.grid(True)
+plt.show()
+
+
+
+"""
+Entre mayor cantidad de hilos, 
+
+"""
